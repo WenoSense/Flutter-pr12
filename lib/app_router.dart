30c32da481092ceed7cc7c5e60d3b45d1ce46c73
Kirill Solomatin <sgaska@bk.ru>
2025-12-12 16:41:58 +0300

    Изменение маршрутной карты

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,19 +1,37 @@
 import 'package:go_router/go_router.dart';
 import 'ui/features/payments/screens/add_payment_screen.dart';
 import 'ui/features/payments/screens/payments_screen.dart';
+import 'ui/features/pin_code/screens/pin_code_screen.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: 'payments',
-      builder: (context, state) => const PaymentsScreen(),
-    ),
-    GoRoute(
-      path: '/add-payment',
-      name: 'add-payment',
-      builder: (context, state) => const AddPaymentScreen(),
-    ),
-  ],
-);
+GoRouter createRouter(bool hasPinCode) {
+  return GoRouter(
+    initialLocation: hasPinCode ? '/pin-verify' : '/pin-create',
+    routes: [
+      GoRoute(
+        path: '/pin-create',
+        name: 'pin-create',
+        builder: (context, state) => const PinCodeScreen(
+          mode: PinCodeMode.create,
+        ),
+      ),
+      GoRoute(
+        path: '/pin-verify',
+        name: 'pin-verify',
+        builder: (context, state) => const PinCodeScreen(
+          mode: PinCodeMode.verify,
+        ),
+      ),
+      GoRoute(
+        path: '/payments',
+        name: 'payments',
+        builder: (context, state) => const PaymentsScreen(),
+      ),
+      GoRoute(
+        path: '/add-payment',
+        name: 'add-payment',
+        builder: (context, state) => const AddPaymentScreen(),
+      ),
+    ],
+  );
+}
+
